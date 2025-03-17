67d467a4f6a5069d99e27e893a403152b15feb9f
Wendyyy21 <yandew718@gmail.com>
2025-03-17 17:49:45 +0800

    Finished chatbot frontend

--- lib/views/pages/elderly/chat_page.dart
+++ lib/views/pages/elderly/chat_page.dart
@@ -10,70 +10,117 @@ class Elderly_ChatPage extends StatefulWidget {
 
 class _Elderly_ChatPageState extends State<Elderly_ChatPage> {
   TextEditingController userInputController = TextEditingController();
+  ScrollController scrollController = ScrollController();
   List<Widget> messages = [
+    SizedBox(height: 10.0),
     AIMessageWidget(
       response: 'Hi, I\'m YoYo! Type in the text box below to start chatting!',
+      isLoading: false,
     ),
   ];
 
   void updateChat(String user_text, String AI_text) {
     setState(() {
+      messages.add(SizedBox(height: 10.0));
       messages.add(userMessageWidget(message: user_text));
     });
-    Future.delayed(Duration(seconds: 3), () {
-      setState(() {
-        messages.add(AIMessageWidget(response: AI_text));
-      });
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (scrollController.hasClients) {
+        scrollController.animateTo(
+          scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
+      }
+    });
+
+    setState(() {
+      messages.add(SizedBox(height: 10.0));
+      messages.add(AIMessageWidget(response: AI_text, isLoading: true));
+    });
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (scrollController.hasClients) {
+        scrollController.animateTo(
+          scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
+      }
     });
   }
 
   @override
   void dispose() {
     userInputController.dispose();
+    scrollController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
+    return Column(
+      children: [
+        Expanded(
+          child: SingleChildScrollView(
+            controller: scrollController,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 0.0,
+                horizontal: 20.0,
+              ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: messages,
               ),
             ),
           ),
-          const SizedBox(height: 10),
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: userInputController,
-                  decoration: const InputDecoration(
-                    hintText: 'Type anything',
-                    border: OutlineInputBorder(),
+        ),
+        const SizedBox(height: 10),
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 245, 243, 243),
+            border: Border(
+              top: BorderSide(
+                color: const Color.fromARGB(255, 222, 220, 220),
+                width: 1.0,
+              ),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 10.0,
+              horizontal: 20.0,
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: userInputController,
+                    decoration: const InputDecoration(
+                      fillColor: Color.fromARGB(255, 253, 252, 252),
+                      filled: true,
+                      hintText: 'Type anything',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(width: 10),
-              IconButton(
-                onPressed: () {
-                  updateChat(
-                    userInputController.text,
-                    'This is a dummy response',
-                  );
-                  userInputController.clear();
-                },
-                icon: const Icon(Icons.send),
-              ),
-            ],
+                const SizedBox(width: 10),
+                IconButton(
+                  onPressed: () {
+                    updateChat(
+                      userInputController.text,
+                      'This is a dummy response', //TODO: replace with actual response
+                    );
+                    userInputController.clear();
+                  },
+                  icon: const Icon(Icons.send),
+                ),
+              ],
+            ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
--- lib/views/widgets/chat_widget.dart
+++ lib/views/widgets/chat_widget.dart
@@ -1,15 +1,82 @@
 import 'package:flutter/material.dart';
 
-class AIMessageWidget extends StatelessWidget {
-  const AIMessageWidget({super.key, required this.response});
+class LoadingDots extends StatefulWidget {
+  const LoadingDots({super.key});
+
+  @override
+  State<LoadingDots> createState() => _LoadingDotsState();
+}
+
+class _LoadingDotsState extends State<LoadingDots>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<int> dotAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(seconds: 3),
+      vsync: this,
+    )..repeat();
+
+    dotAnimation = IntTween(
+      begin: 1,
+      end: 3,
+    ).animate(CurvedAnimation(parent: controller, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: dotAnimation,
+      builder: (context, child) {
+        String dots = '.' * dotAnimation.value;
+        return Text(
+          dots,
+          style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.w900),
+        );
+      },
+    );
+  }
+}
+
+class AIMessageWidget extends StatefulWidget {
+  AIMessageWidget({super.key, required this.response, required this.isLoading});
   final String response;
+  bool isLoading;
+
+  @override
+  State<AIMessageWidget> createState() => _AIMessageWidgetState();
+}
+
+class _AIMessageWidgetState extends State<AIMessageWidget> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      setState(() {
+        widget.isLoading = false;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        CircleAvatar(radius: 40.0),
+        CircleAvatar(
+          radius: 40.0,
+          foregroundImage: AssetImage('assets/images/yoyo_avatar.jpg'),
+        ),
         SizedBox(width: 10.0),
         ConstrainedBox(
           constraints: BoxConstraints(
@@ -20,7 +87,10 @@ class AIMessageWidget extends StatelessWidget {
           child: Card(
             child: Padding(
               padding: const EdgeInsets.all(10.0),
-              child: Text(response, style: TextStyle(fontSize: 20.0)),
+              child:
+                  widget.isLoading
+                      ? LoadingDots()
+                      : Text(widget.response, style: TextStyle(fontSize: 20.0)),
             ),
           ),
         ),
@@ -52,7 +122,10 @@ class userMessageWidget extends StatelessWidget {
           ),
         ),
         SizedBox(width: 10.0),
-        CircleAvatar(radius: 40.0),
+        CircleAvatar(
+          radius: 40.0,
+          foregroundImage: AssetImage('assets/images/elderly_avatar.png'),
+        ),
       ],
     );
   }
